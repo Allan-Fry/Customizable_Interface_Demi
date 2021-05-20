972ba67c04d1af3a0d075be0459c6fd54f3f4bec
Allan-Fry <allan.fry.it@gmail.com>
2021-05-20 23:22:29 +1200

    Completed move from Complete to wip

--- app_drag_drop_demo/src/components/AppDragDropDemo/AppDragDropDemo.jsx
+++ app_drag_drop_demo/src/components/AppDragDropDemo/AppDragDropDemo.jsx
@@ -26,7 +26,28 @@ export default class AppDragDropDemo extends Component
     onDragOver = (ev) => {
         ev.preventDefault();
     }
-    
+
+    onDragStart = (ev, id) => {
+        console.log('dragstart:',id);
+        ev.dataTransfer.setData("id", id);
+    }
+    onDrop = (ev, cat) => {
+        let id = ev.dataTransfer.getData("id");
+
+        let tasks = this.state.tasks.filter((task) => {
+            if (task.name === id) {
+                task.category = cat;
+            }
+            return task;
+        });
+
+        this.setState({
+            ...this.state,
+            tasks
+        });
+    }    
+
+
     render () 
     {    
         
@@ -40,39 +61,55 @@ export default class AppDragDropDemo extends Component
         this.state.tasks.forEach((t) => 
         {
           tasks[t.category].push(
-            <Toast    
+            <Row>
+            <Col />
+                <Toast    
                     key={t.name}
-                    
                     draggable
+                    onDragStart = {(e) => this.onDragStart(e, t.name)}
                     className="draggable"
                     style={{backgroundColor: t.bgcolor}}
             >
-                <ToastHeader>{t.name}</ToastHeader>
-            </Toast>
+                    <ToastHeader>
+                        {t.name}
+                    </ToastHeader>
+                </Toast>
+            <Col />
+            </Row>
           )  
         });
         
 
         return (      
-            <Container className="container-drag">        
-                <h2 className="header">
-                    DRAG & DROP DEMO
-                </h2>
-                <Row className="wip">
-                    <Col className="task-header">
-                        WIP
+            <Container className="container-drag">    
+                <Row>
+                    <Col tag='h2' className="header">
+                        DRAG & DROP DEMO
                     </Col>
-                    {tasks.wip}
                 </Row>
-                <Row 
-                    className="droppable" 
-                    onDragOver={
-                        (e)=>this.onDragOver(e)}
-                >
-                    <Col className="task-header">
-                       COMPLETED
-                    </Col> 
-                    {tasks.complete}
+                <Row>
+                    <Col className="wip"
+                    onDragOver={(e)=>this.onDragOver(e)}
+                    onDrop={(e)=>{this.onDrop(e, "wip")}}>
+                        <Row>
+                            <Col tag='h3' className="task-header">
+                                WIP
+                            </Col>
+                        </Row>
+                            {tasks.wip}
+                    </Col>
+                    <Col />
+                    <Col className="droppable" 
+                    onDragOver={(e)=>this.onDragOver(e)}
+                    onDrop={(e)=>this.onDragOver(e, "complete")}>
+                           
+                        <Row>
+                            <Col tag='h3' className="task-header">
+                                COMPLETED
+                            </Col> 
+                        </Row>
+                            {tasks.complete}
+                    </Col>
                 </Row>
             </Container>
             
