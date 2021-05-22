d783f3f7f1aca6dbb3afe35118047a069dc79b99
Allan-Fry <allan.fry.it@gmail.com>
2021-05-23 11:43:16 +1200

    Reformated to have toast stored in an array

--- app_drag_drop_demo/src/components/AppDragDropDemo/AppDragDropDemo.jsx
+++ app_drag_drop_demo/src/components/AppDragDropDemo/AppDragDropDemo.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Toast, ToastHeader, Container, Row, Col} from 'reactstrap';
+import { Container, Row, Col} from 'reactstrap';
 import './AppDragDropDemo.css';
 
 export default class AppDragDropDemo extends Component{  
@@ -18,41 +18,43 @@ export default class AppDragDropDemo extends Component{
          unPlaced:[]
 
         }
-        
+        tasks['unPlaced'].push(
+            this.props.toasts
+        );
 
-        this.props.tasks.forEach((t) => 
-        {
-          tasks["unPlaced"].push(
-            <Row>
-            <Col />
-                <Toast    
-                    key={t.name}
-                    draggable
-                    onDragStart = {(e) => this.onDragStart(e, t.name)}
-                    className="draggable"
-                    style={{backgroundColor: t.bgcolor}}
-            >
-                    <ToastHeader>
-                        {t.name}
-                    </ToastHeader>
-                </Toast>
-            <Col />
-            </Row>
-          )  
-        });
+        // this.props.tasks.forEach((t) => 
+        // {
+        //   tasks["unPlaced"].push(
+        //     <Row>
+        //     <Col />
+        //         <Toast    
+        //             key={t.name}
+        //             draggable
+        //             onDragStart = {(e) => this.onDragStart(e, t.name)}
+        //             className="draggable"
+        //             style={{backgroundColor: t.bgcolor}}
+        //     >
+        //             <ToastHeader>
+        //                 {t.name}
+        //             </ToastHeader>
+        //         </Toast>
+        //     <Col />
+        //     </Row>
+        //   )  
+        // });
         
 
         return (      
             <Container className="container-drag">    
                 <Row>
                     <Col className="wip"
-                    onDrop={(e)=>{this.onDrop(e, "unPlaced")}}>
+                    >
                         <Row>
                             <Col tag='h3' className="task-header">
                                 Unplaced
                             </Col>
                         </Row>
-                            {tasks.unPlaced}
+                           {tasks.unPlaced}
                     </Col>
                     <Col />
                 </Row>
